37902c6ca3d6ec26e25584d72ee71abd5b1ab5f5
Jodi N Kansagor <joedvice@gmail.com>
2020-02-14 16:35:06 -0800

    repo reducer test pass

--- src/reducers/repoReducer.test.js
+++ src/reducers/repoReducer.test.js
@@ -1,8 +1,16 @@
-import { FETCH_USERREPOS, FETCH_USERREPOS_LOADING } from "../actions/userReposAction"
+import { FETCH_USERREPOS, FETCH_USERREPOS_LOADING } from '../actions/userReposAction';
+import reducer from './repoReducer';
 
 describe('repo reducer', () => {
   it('handles fetching user repos', () => {
     const action = { type: FETCH_USERREPOS_LOADING };
-    const initial state
-  })
-})
+    const initialState = { loading: false, userRepos: [] };
+
+    const newState = reducer(initialState, action);
+
+    expect(newState).toEqual({
+      loading: true,
+      userRepos: []
+    });
+  });
+});
